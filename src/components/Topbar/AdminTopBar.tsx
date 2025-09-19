854abcddb5289b8db0bbaf0240bf2c221fb205c8
chiraswykj <chiraswy@gmail.com>
2025-09-19 14:38:17 +0530

    commit

--- src/components/Topbar/AdminTopBar.tsx
+++ src/components/Topbar/AdminTopBar.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { NavLink, useNavigate } from "react-router-dom"
+import { Button } from "../ui/button"
+import { Power } from "lucide-react"
 
 const AdminTopBar = () => {
-  return (
-    <div className='h-16 w-svw bg-primary fixed top-0 left-0 text-white flex justify-center items-center'>AdminTopBar</div>
-  )
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        navigate('/')
+    }
+    return (
+        <div className="h-14 w-svw  text-primary bg-white 
+    flex justify-center items-center border-b  border-violet-600 shadow-sm
+     shadow-purple-600 px-4">
+            <div className="w-[50%] h-full flex flex-row justify-start items-center gap-4">
+                <NavLink to='/admin/products' className='h-full flex justify-center items-center font-bold text-xl px-3'> Products</NavLink>
+                <NavLink to='/admin/users' className='h-full flex justify-center items-center font-bold text-xl px-3'> Users</NavLink>
+                <NavLink to='/admin/orders' className='h-full flex justify-center items-center font-bold text-xl px-3'> Orders</NavLink>
+            </div>
+            <div className="w-[50%] h-full flex flex-row justify-end items-center gap-4">
+                <Button className="bg-red-500 hover:bg-red-400 cursor-pointer" onClick={handleLogout}>
+                    <Power />
+                </Button>
+            </div>
+        </div>
+    )
 }
 
 export default AdminTopBar
--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,17 +1,15 @@
 import AdminTopBar from "@/components/Topbar/AdminTopBar"
 import { Outlet } from "react-router-dom"
 
-
 const AdminLayout = () => {
-  return (
-    <div className="flex justify-start items-center flex-col">
-      <AdminTopBar />
-      <main>
-        <Outlet />
-      </main>
-      
-    </div>
-  )
+    return (
+        <div className="flex justify-start items-center flex-col">
+            <AdminTopBar />
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    )
 }
 
 export default AdminLayout
